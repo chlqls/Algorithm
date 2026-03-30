0c16376da03ee3a8c92d4eb12247a09b83fb0106
beanii <99929277+chlqls@users.noreply.github.com>
2026-03-30 12:31:17 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, sum = 0;
+    cin >> N;
+
+    vector<int> arr(N);
+
+    for(int i = 0; i< N; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < N; i++) {
+        sum += arr[i] * (N - i);
+    }
+
+    cout << sum << '\n';
+    
+    return 0;
+}
