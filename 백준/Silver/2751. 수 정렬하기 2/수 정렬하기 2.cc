c0ceb74e2ea02f28ff89e7d8e6758de078fc3791
beanii <99929277+chlqls@users.noreply.github.com>
2026-04-02 15:38:05 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 248 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,60 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> sorted;
-
-void merge(vector<int>& arr, int left, int mid, int right) {
-    int pl = left, pr = mid + 1, idx = left;
-    while (pl <= mid && pr <= right)
-    {
-        if(arr[pl] < arr[pr]) {
-            sorted[idx++] = arr[pl++];
-        }
-        else {
-            sorted[idx++] = arr[pr++];
-        }
-    }
-    while(pl <= mid) {
-        sorted[idx++] = arr[pl++];
-    }
-    while(pr <= right) {
-        sorted[idx++] = arr[pr++];
-    }
-    for(int i = left; i <= right; i++) {
-        arr[i] = sorted[i];
-    }
-    
-}
-
-void mergesort(vector<int> &arr, int left, int right) {
-    if(left < right) {
-        int mid = (left + right) / 2;
-        mergesort(arr, left, mid);
-        mergesort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
-}
-
 int main() {
-    int n;
 
-    //입력
-    cin >> n;
-    vector<int> arr(n);
-    sorted.assign(n, 0);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
     }
 
-    //연산
-    mergesort(arr, 0, n - 1);
+    sort(A.begin(), A.end());
 
-    //출력
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << '\n';
+    for (int v : A) {
+        cout << v << '\n';
     }
 
     return 0;
